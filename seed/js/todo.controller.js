46b19ee08244920bec0ed331b73e6a3cf0312cc6
Jennifer Walker <dataqueen16@gmail.com>
2018-09-13 19:25:44 -0500

    creating a todo list, added a checkbox for each

---
+++ seed/js/todo.controller.js
@@ -0,0 +1,19 @@
+function ToDoController() {
+  this.list = [{
+    title: 'First todo item',
+    completed: 'false'
+  },
+    {
+      title: 'Second todo item',
+      completed: 'false'
+    },
+    {
+      title: 'Third todo item',
+      completed: 'false'
+    }
+  ];
+}
+
+angular
+  .module('app')
+  .controller('ToDoController', ToDoController);
